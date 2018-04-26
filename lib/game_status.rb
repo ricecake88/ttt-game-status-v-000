4e2566945478c04807c4cc51a735015925d7d0ee
Grace Shih <vocalise@gmail.com>
2018-04-26 07:43:12 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -35,7 +35,7 @@ def won?(board)
 end
 
 def full?(board) 
-  board.all? {|element| element == "X" || element == "O")}
+  board.all? {|element| element == "X" || element == "O"}
 end
   
 board = ["X", " ", " ", "X", " ", " ", "X", " ", " "]
